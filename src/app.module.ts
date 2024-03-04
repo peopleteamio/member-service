d0597ae2f830647dece5502881e3164b5f9d5788
TheSpagi <thespagi@icloud.com>
2024-03-04 01:21:53 +0100

    Add DatabaseModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
+import { DatabaseModule } from "./shared/database/database.module";
 
 const config = ConfigModule.forRoot();
+const modules = [DatabaseModule];
 
 @Module({
-  imports: [config],
+  imports: [config, ...modules],
   controllers: [AppController],
   providers: [AppService]
 })
